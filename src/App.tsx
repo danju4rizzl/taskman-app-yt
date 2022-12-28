671a2e9c11967d94659d449cea2409120c67725b
danju4rizzl <danjumashiwaju@gmail.com>
2022-12-28 02:13:11 +0200

    added uuid pkg and setting up local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, {
-    ChangeEvent,
-    FormEvent,
-    TextareaHTMLAttributes,
-    useState,
+	ChangeEvent,
+	FormEvent,
+	TextareaHTMLAttributes,
+	useState
 } from "react";
 import "./App.css";
 import bgImage from "./assets/bg.jpg";
@@ -10,58 +10,44 @@ import TaskForm from "./components/TaskForm";
 import TaskItem from "./components/TaskItem";
 
 function Container(props: { child: JSX.Element[] | JSX.Element }) {
-    return (
-        <div className='flex place-items-center px-5 sm:px-10 md:px-32'>
-            {props.child}
-        </div>
-    );
+	return (
+		<div className="flex place-items-center px-5 sm:px-10 md:px-32">
+			{props.child}
+		</div>
+	);
 }
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [formState, setFormState] = useState("");
-    const [task, setTask] = useState([]);
-
-    function handleSubmit() {
-        console.log("Element", formState);
-        //Deejay update states here
-    }
-
-    function checker(e: string) {
-        console.log("e is", e);
-        setFormState(e);
-    }
-
-    return (
-        <div
-            className={`h-screen grid grid-flow-row lg:grid-flow-col content-center bg-no-repeat bg-cover bg-center text-white px-5 md:px-28 space-y-5`}
-            style={{
-                backgroundImage: `linear-gradient(
+	return (
+		<div
+			className={`h-screen grid grid-flow-row lg:grid-flow-col content-center bg-no-repeat bg-cover bg-center text-white px-5 md:px-28 space-y-5`}
+			style={{
+				backgroundImage: `linear-gradient(
                                     rgba(62, 62, 62, 0.9),
                                     rgba(62, 62, 62, 0.9)),
-                                    url(${bgImage})`,
-            }}
-        >
-            <div className='grid px-5 sm:px-10 md:px-24'>
-                <div className='text-center my-12 space-y-1'>
-                    <h1 className='text-5xl'>Task Manager</h1>
-                    <p>A simple way to manage your tasks, anywhere</p>
-                </div>
-                {/* fix the TaskForm here*/}
-                <TaskForm onClick={handleSubmit} onChange={checker} />
-            </div>
-            <div className='my-auto'>
-                <ul className=' h-48 md:h-80 flex flex-col pr-4 sm:pl-10 md:pl-24 space-y-3 md:space-y-3 overflow-y-scroll'>
-                    {task.length > 0 ? (
-                        task.map((item) => <TaskItem title={item.t} />)
-                    ) : (
-                        <p>No Task</p>
-                    )}
-                </ul>
-                <p>na here</p>
-            </div>
-        </div>
-    );
+                                    url(${bgImage})`
+			}}
+		>
+			<div className="grid px-5 sm:px-10 md:px-24">
+				<div className="text-center my-12 space-y-1">
+					<h1 className="text-5xl">Task Manager</h1>
+					<p>A simple way to manage your tasks, anywhere</p>
+				</div>
+
+				<TaskForm />
+			</div>
+			<div className="my-auto">
+				<ul className=" h-48 md:h-80 flex flex-col pr-4 sm:pl-10 md:pl-24 space-y-3 md:space-y-3 overflow-y-scroll">
+					{/* {task.length > 0 ? (
+						task.map((item) => <TaskItem title={item.t} />)
+					) : (
+						<p>No Task</p>
+					)} */}
+				</ul>
+				<p>na here</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,27 +1,52 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-type TaskFormProps = {
-    onClick: () => void; // fix here
-    onChange: (e: any) => void;
-};
+export default function TaskForm() {
+	const [inputState, setInputState] = useState("");
+	const [task, setTask] = useState([{}]);
+	const isEmpty = inputState.length; // This will check if the input is empty or not
 
-export default function TaskForm(props: TaskFormProps) {
-    return (
-        <div className='grid space-y-5'>
-            <textarea
-                rows={5}
-                name='taskInput'
-                placeholder='Feed Bingo at 3pm'
-                className='py-2 px-5 text-2xl rounded-lg text-center border outline-1 outline-current  bg-slate-700/50'
-                onChange={(e: any) => props.onChange(e.target.value)}
-            />
-            <button
-                type='button'
-                className={`py-2 hover:bg-red-400 bg-red-500 ease-in duration-300 rounded-md`}
-                onClick={() => props.onClick} // fix here
-            >
-                Save Task
-            </button>
-        </div>
-    );
+	function handleSubmit() {
+		if (!isEmpty) return;
+		setInputState(""); // to reset the form input
+		setTask([...task, { id: uuidv4(), text: inputState }]); // Change the state
+	}
+
+	useEffect(() => {
+		localStorage.setItem("storedTask", JSON.stringify(task)); // Store the state in local storage
+	}, [task]);
+
+	//    @danjuma Gets the local Data Continue here store the data to local storage
+	// function getLocalData() {
+	// 	const ls = localStorage.getItem("storedTask");
+	// 	const currentData = JSON.parse(ls);
+	// 	console.log(currentData);
+	// }
+
+	return (
+		<div className="grid space-y-5">
+			<textarea
+				rows={5}
+				value={inputState}
+				name="taskInput"
+				placeholder="Feed Bingo at 3pm"
+				className="py-2 px-5 text-2xl rounded-lg text-center border outline-1 outline-current  bg-slate-700/50"
+				onChange={(e) => setInputState(e.target.value)}
+			/>
+			<p className="text-sm">
+				Characters: {!isEmpty ? "None 🔴" : `${isEmpty} 🟢`}
+			</p>
+			<button
+				type="button"
+				className={`py-2  ease-in duration-300 rounded-md ${
+					isEmpty
+						? " hover:bg-red-400 bg-red-500"
+						: "  bg-gray-500 cursor-not-allowed"
+				} `}
+				onClick={handleSubmit}
+			>
+				Save Task
+			</button>
+		</div>
+	);
 }
